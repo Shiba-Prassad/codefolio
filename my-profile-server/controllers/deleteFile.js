0d689716646cd6d1d8dd2be3855a9181b65710dd
Shiba-Prassad <140606262+Shiba-Prassad@users.noreply.github.com>
2023-09-22 15:08:50 +0530

    Update deleteFile.js
    console

--- my-profile-server/controllers/deleteFile.js
+++ my-profile-server/controllers/deleteFile.js
@@ -19,7 +19,7 @@ const deleteFile = asyncHandler(async (req, res) => {
   // Use fs.unlink to delete the file
   fs.unlink(filePath, (error) => {
     if (error) {
-      res.status(500).json({ error: "Failed to delete the file: " + error });
+      res.status(500).json({ error: process.env.NODE_ENV + "Failed to delete the file: " + error });
     } else {
       res.json({ message: "File deleted successfully" });
     }
